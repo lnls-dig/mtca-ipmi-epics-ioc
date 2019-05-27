117475b7a0df09ebb56876bf3f4133373a18649a
Henrique Silva <henrique.silva@lnls.br>
2019-05-27 14:48:15 +0000

    iocBoot/ioc-mtca-common/*.cmd: Remove archiver dependency

--- iocBoot/ioc-mtca-common/st_mtca_common.cmd
+++ iocBoot/ioc-mtca-common/st_mtca_common.cmd
@@ -20,8 +20,6 @@ dbLoadRecords("db/cooling_unit.template","P=$(CRATE),S=$(CU2=CU02:),UNIT=2")
 dbLoadRecords("db/power_modules.db","P=$(CRATE)")
 dbLoadRecords("db/mch.db","P=$(CRATE),PM=$(PM=PM02:)")
 
-< $(TOP)/iocBoot/archiver_tags.cmd
-
 iocInit()
 
 cd $(TOP)
