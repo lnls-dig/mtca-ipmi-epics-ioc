2797c10d15db08559c591766ad5e3e5b3fea4074
Henrique Silva <henrique.silva@lnls.br>
2019-05-28 14:22:52 +0000

    mtcaSensorsApp/Db/*: Use LNLS naming convention for PVs

--- iocBoot/ioc-mtca-common/st_mtca_common.cmd
+++ iocBoot/ioc-mtca-common/st_mtca_common.cmd
@@ -13,12 +13,12 @@ mtcaSensors_registerRecordDeviceDriver pdbbase
 ## Load record instances
 # Set CU1, CU2 environment variables to override default cooling unit names.
 # Set PM environment variables to override default power module name.
-dbLoadRecords("db/mtca_crate.db","P=$(CRATE),MCH_HOST=$(MCH_HOST),CRATE_ID=$(CRATE_ID),RACK_ID=$(RACK_ID),CU1=$(CU1=CU01:),CU2=$(CU2=CU02:)")
-dbLoadRecords("db/amc_cards.db","P=$(CRATE),PM=$(PM=PM02:)")
-dbLoadRecords("db/cooling_unit.template","P=$(CRATE),S=$(CU1=CU01:),UNIT=1")
-dbLoadRecords("db/cooling_unit.template","P=$(CRATE),S=$(CU2=CU02:),UNIT=2")
+dbLoadRecords("db/mtca_crate.db","P=$(CRATE),MCH_HOST=$(MCH_HOST),CRATE_ID=$(CRATE_ID),RACK_ID=$(RACK_ID),CU1=$(CU1=CU01),CU2=$(CU2=CU02)")
+dbLoadRecords("db/amc_cards.db","P=$(CRATE),PM=$(PM=PM02)")
+dbLoadRecords("db/cooling_unit.template","P=$(CRATE),S=$(CU1=CU01),UNIT=1")
+dbLoadRecords("db/cooling_unit.template","P=$(CRATE),S=$(CU2=CU02),UNIT=2")
 dbLoadRecords("db/power_modules.db","P=$(CRATE)")
-dbLoadRecords("db/mch.db","P=$(CRATE),PM=$(PM=PM02:)")
+dbLoadRecords("db/mch.db","P=$(CRATE),PM=$(PM=PM02)")
 
 iocInit()
 
