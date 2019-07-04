61f9e76542f423227dd5678092abb47ac7413639
Lucas Russo <lerwys@gmail.com>
2019-07-04 10:56:47 -0300

    iocBoot: add st_mtca_common.cmd directly into general iocMTCAIpmi

--- iocBoot/iocMTCAIpmi/st.cmd
+++ iocBoot/iocMTCAIpmi/st.cmd
@@ -1,14 +1,28 @@
-#!../../bin/linux-x86_64/mtcaSensors
+# Startup script for MTCA crate monitoring IOC
+< envPaths
 
-## You may have to change mtcaSensors to something else
-## everywhere it appears in this file
-
+epicsEnvSet("TOP","../..")
 epicsEnvSet("CRATE", "DIG:MCH_TEST:")
 epicsEnvSet("MCH_HOST", "10.2.118.35")
 epicsEnvSet("CRATE_ID", "MTCA RSV")
 epicsEnvSet("RACK_ID", "Dev net")
 
-< envPaths
+< MTCA_IPMI.config
 
-< $(TOP)/iocBoot/ioc-mtca-common/st_mtca_common.cmd
+## Register all support components
+dbLoadDatabase "dbd/mtcaSensors.dbd"
+mtcaSensors_registerRecordDeviceDriver pdbbase
 
+## Load record instances
+# Set CU1, CU2 environment variables to override default cooling unit names.
+# Set PM environment variables to override default power module name.
+dbLoadRecords("db/mtca_crate.db","P=$(CRATE),MCH_HOST=$(MCH_HOST),CRATE_ID=$(CRATE_ID),RACK_ID=$(RACK_ID),CU1=$(CU1=CU01),CU2=$(CU2=CU02)")
+dbLoadRecords("db/amc_cards.db","P=$(CRATE),PM=$(PM=PM02)")
+dbLoadRecords("db/cooling_unit.template","P=$(CRATE),S=$(CU1=CU01),UNIT=1")
+dbLoadRecords("db/cooling_unit.template","P=$(CRATE),S=$(CU2=CU02),UNIT=2")
+dbLoadRecords("db/power_modules.db","P=$(CRATE)")
+dbLoadRecords("db/mch.db","P=$(CRATE),PM=$(PM=PM02)")
+
+iocInit()
+
+# This IOC can take >20 s to exit. Please be patient.
