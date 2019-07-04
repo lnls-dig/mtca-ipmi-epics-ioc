b60da18718ba809f634a6d5941c834161d7035e0
Lucas Russo <lerwys@gmail.com>
2019-07-04 17:08:02 -0300

    iocBoot: add TOP prefix to all dbLoadRecords

--- iocBoot/iocMTCAIpmi/stMTCACrate12.cmd
+++ iocBoot/iocMTCAIpmi/stMTCACrate12.cmd
@@ -12,12 +12,12 @@ mtcaSensors_registerRecordDeviceDriver pdbbase
 ## Load record instances
 # Set CU1, CU2 environment variables to override default cooling unit names.
 # Set PM environment variables to override default power module name.
-dbLoadRecords("db/mtca_crate.db","P=$(CRATE),MCH_HOST=$(MCH_HOST),CRATE_ID=$(CRATE_ID),RACK_ID=$(RACK_ID),CU1=$(CU1=CU01),CU2=$(CU2=CU02)")
-dbLoadRecords("db/amc_cards.db","P=$(CRATE),PM=$(PM=PM02)")
-dbLoadRecords("db/cooling_unit.template","P=$(CRATE),S=$(CU1=CU01),UNIT=1")
-dbLoadRecords("db/cooling_unit.template","P=$(CRATE),S=$(CU2=CU02),UNIT=2")
-dbLoadRecords("db/power_modules.db","P=$(CRATE)")
-dbLoadRecords("db/mch.db","P=$(CRATE),PM=$(PM=PM02)")
+dbLoadRecords("${TOP}/db/mtca_crate.db","P=$(CRATE),MCH_HOST=$(MCH_HOST),CRATE_ID=$(CRATE_ID),RACK_ID=$(RACK_ID),CU1=$(CU1=CU01),CU2=$(CU2=CU02)")
+dbLoadRecords("${TOP}/db/amc_cards.db","P=$(CRATE),PM=$(PM=PM02)")
+dbLoadRecords("${TOP}/db/cooling_unit.template","P=$(CRATE),S=$(CU1=CU01),UNIT=1")
+dbLoadRecords("${TOP}/db/cooling_unit.template","P=$(CRATE),S=$(CU2=CU02),UNIT=2")
+dbLoadRecords("${TOP}/db/power_modules.db","P=$(CRATE)")
+dbLoadRecords("${TOP}/db/mch.db","P=$(CRATE),PM=$(PM=PM02)")
 
 iocInit()
 
